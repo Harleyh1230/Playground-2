31b7f09e5adef25b705e4d1a927f5215f9bfdadb
Harley Hutchinson <Harley.Hutchinson@inphase.com>
2024-03-26 10:33:28 +0000

    Added border around the CustomFormWithDatePicker component

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
       <section className="mb-8">
       <CustomDatePicker /> 
       </section>
-      <section className="mb-8">
+      <section className="mb-8 border border-green-500 py-8 rounded-lg">
+        <h1 className="text-2xl font-bold flex justify-center">Custom Form With DatePicker</h1>
         <CustomFormWithDatePicker/>
       </section>
     </main>
